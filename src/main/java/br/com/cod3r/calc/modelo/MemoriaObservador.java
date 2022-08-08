2bbe62681e4c03c0fb1f7eb1a4d7b49aa1ef538a
Cícero Leão <ccleao9@gmail.com>
2022-08-08 10:35:48 -0300

    Calculadora Básica Final

---
+++ src/main/java/br/com/cod3r/calc/modelo/MemoriaObservador.java
@@ -0,0 +1,6 @@
+package br.com.cod3r.calc.modelo;
+
+public interface MemoriaObservador {
+
+    public void valorAlterado(String novoValor);
+}
---
+++ src/main/java/br/com/cod3r/calc/visao/Display.java
@@ -0,0 +1,30 @@
+package br.com.cod3r.calc.visao;
+
+import br.com.cod3r.calc.modelo.Memoria;
+import br.com.cod3r.calc.modelo.MemoriaObservador;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Display extends JPanel implements MemoriaObservador {
+
+    private final JLabel label;
+
+    public Display() {
+        Memoria.getInstancia().adicionarObservador(this);
+
+        setBackground(new Color(46, 49, 50));
+        label = new JLabel(Memoria.getInstancia().getTextoAtual());
+        label.setForeground(Color.WHITE);
+        label.setFont(new Font("courier", Font.PLAIN, 30));
+
+        setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25));
+
+        add(label);
+    }
+
+    @Override
+    public void valorAlterado(String novoValor) {
+        label.setText(novoValor);
+    }
+}
